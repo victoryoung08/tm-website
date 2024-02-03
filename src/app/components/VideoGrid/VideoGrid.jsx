4b2514558d831d57d5148454bc75128ee04a358d
victoryoung <vicmachine08@gmail.com>
2024-02-04 08:04:30 +1100

    error handling

--- src/app/components/VideoGrid/VideoGrid.jsx
+++ src/app/components/VideoGrid/VideoGrid.jsx
@@ -2,16 +2,20 @@ import { getFirstSixVideos } from "@/app/utils/vimeo";
 import VideoModal from "@/app/components/VideoModal/VideoModal";
 export default async function VideoGrid() {
   const videos = await getFirstSixVideos();
-  return (
-    <div className="grid grid-cols-3 grid-rows-3 gap-1 w-fit mx-auto max-h-[600px] md:max-w-[600px]">
-      {videos.map((video, index) => (
-        <div
-          key={video.uri}
-          className="h-full w-full rounded-md overflow-hidden"
-        >
-          <VideoModal video={video} />
-        </div>
-      ))}
-    </div>
-  );
+  if (videos) {
+    return (
+      <div className="grid grid-cols-3 grid-rows-3 gap-1 w-fit mx-auto max-h-[600px] md:max-w-[600px]">
+        {videos.map((video, index) => (
+          <div
+            key={video.uri}
+            className="h-full w-full rounded-md overflow-hidden"
+          >
+            <VideoModal video={video} />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    <p>Error when fetching Videos</p>;
+  }
 }
